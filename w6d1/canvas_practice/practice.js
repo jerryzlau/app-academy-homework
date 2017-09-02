386da5aeb5067b287b8bde07fd4c1e56be3b9732
Jerry Lau <jerryzlau@gmail.com>
2017-09-02 15:49:03 -0700

    Finished canvas homework

---
+++ w6d1/canvas_practice/practice.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function(){
+  const c = document.getElementById("mycanvas");
+  c.height = 500;
+  c.width = 500;
+
+  const ctx = c.getContext("2d");
+  ctx.fillStyle = "red";
+  ctx.fillRect(0,0,500,500);
+
+  ctx.beginPath();
+  ctx.arc(100, 200, 40, 0 , 2*Math.PI);
+  ctx.strokeStyle = "blue";
+  ctx.lineWidth = 10;
+  ctx.stroke();
+  ctx.fillStyle = "green";
+  ctx.fill();
+
+});
